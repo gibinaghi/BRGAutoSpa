01d0a32d00a0b41ebddf4fa655f7e23e11bb1ea2
Gimena Binaghi <gimenabinaghi@hotmail.com>
2022-03-24 14:17:16 -0300

    cerrar session

--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,15 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic import View
-
+from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 
 class VRegistro(View):
     
     def get(self, request):
+        #Muestra el formulario
         form=UserCreationForm()
         return render(request, 'registro/registro.html', {'form':form})
 
     def post(self, request):
-        pass
+        #Gestiona envio de información a traves del formulario
+        form=UserCreationForm(request.POST)
+
+        if form.is_valid():
+            usuario = form.save() #almacena la información del formulario en la base de datos
+            login(request, usuario) 
+            return redirect('Home')
+        else: 
+            for msg in form.error_messages:
+                messages.error(request, form.error_messages[msg])
+            
+            return render(request, 'registro/registro.html', {'form':form})
+
+def cerrar_sesion(request):
+    logout(request)
+    return redirect('Home')
+        
+
