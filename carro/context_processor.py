ef2224c3025c845a70b8eeb9f9e67a05f08dc73c
Gimena Binaghi <gimenabinaghi@hotmail.com>
2022-05-08 19:55:53 -0300

    arreglando carro

--- carro/context_processor.py
+++ carro/context_processor.py
@@ -3,7 +3,10 @@
 def importe_total_carro(request):
     total=0
 
-    #if request.user.is_authenticated:
-    """for key, value in request.session["carro"].items():
-        total=total+(float(value["precio"]))"""
+    if request.user.is_authenticated:
+        for key, value in request.session["carro"].items():
+            total=total+(float(value["precio"]))
+    else: 
+        total="Debes hacer login para ver el carro"
+
     return{"importe_total_carro":total}
